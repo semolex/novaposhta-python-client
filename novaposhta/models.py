734ce2a81358a774a13516975e1dc8572d8135d7
osemenshchuk <osemenshchuk@rmn.com>
2015-10-02 19:30:35 +0300

    doc updates, code review

--- novaposhta/models.py
+++ novaposhta/models.py
@@ -30,12 +30,18 @@ class NovaPoshtaApi(object):
         It uses `urllib2` and `json` libs for requests to API through `HTTP` protocol.
         Modifies API template and then makes request to API endpoint.
 
-        :param method (string or unicode):
+        :param method:
             name of the API method, should be passed for every request
-        :param method_props (dict (key: str, value: str)):
+        :type method:
+            str or unicode
+        :param method_props:
             additional params for API methods.
+        :type method_props:
+            dict
         :return:
-             dict: dictionary with fetched data
+            dictionary with fetched info
+        :rtype:
+            dict
         """
         self.query['calledMethod'] = method
         if method_props:
@@ -47,71 +53,77 @@ class NovaPoshtaApi(object):
 
 class Address(NovaPoshtaApi):
     """A class representing the Address model of Nova Poshta API.
-    Uses API methods for parsing, like `getCities`, `getStreet` etc.
-    Used for parsing `geo` data like cities, streets etc.
+    Used for parsing `geodata` like cities, streets etc.
     """
 
     def get_city_ref(self, city):
         """
-        Method for parsing `CityRef` data (unique city hash) from API, required for other methods.
-        Uses `get_city_by_name` method.
-        API method: `getCities`.
+        Method for fetching `CityRef` (unique city hash) from API, required for other methods.
 
-        :Usage:
-         address = Address()
-         address.get_city_ref(city=u'Здолбунів')
-        :param city (str or unicode):
+        :example:
+            ``address = Address()``
+            ``address.get_city_ref(city=u'Здолбунів')``
+        :param city:
             name of the required city
+        :type city:
+            str or unicode
         :return:
-            unicode: parsed `CityRef` value (hash)
+            parsed `CityRef` value (hash)
+        :rtype:
+            unicode
         """
         city_ref = self.get_city_by_name(city=city)['data'][0]['Ref']
         return city_ref
 
     def get_cities(self):
         """
-        Method for parsing all cities data.
-        API method: `getCities`.
+        Method for fetching info about all cities.
 
-        :Usage:
-            address = Address()
-            address.get_cities()
+        :example:
+            ``address = Address()``
+            ``address.get_cities()``
         :return:
-            dict: parsed dictionary of all cities data
+            dictionary with info about all cities
+        :rtype:
+            dict
         """
         req = self.send(method='getCities')
         return req
 
     def get_city_by_name(self, city):
         """
-        Method for parsing city data by city name.
-        API method: `getCities`.
-        API method properties: `FindByString`.
+        Method for fetching info about city by city name.
 
-        :Usage:
-            address = Address()
-            address.get_city_by_name(city=u'Здолбунів')
-        :param city (str or unicode):
+        :example:
+            ``address = Address()``
+            ``address.get_city_by_name(city=u'Здолбунів')``
+        :param city:
             name of the desired city
+        :type city:
+            str or unicode
         :return:
-            dict: parsed dictionary with city data
+            dictionary with info about city
+        :rtype:
+            dict
         """
         req = self.send(method='getCities', method_props={'FindByString': city})
         return req
 
     def ex_get_streets(self, city):
         """
-        Method for parsing all streets data at provided city.
-        API method: `getStreet`.
-        API method properties: `CityRef`.
+        Method for fetching all info about streets in provided city.
 
-        :Usage:
-            address = Address()
-            address.get_streets(city=u'Здолбунів')
-        :param city (str or unicode):
+        :example:
+            ``address = Address()``
+            ``address.get_streets(city=u'Здолбунів')``
+        :param city:
             name of the desired city
+        :type city:
+            str or unicode
         :return:
-            dict: parsed dictionary with streets data
+            dictionary with info about streets
+        :rtype:
+            dict
         """
         city_ref = self.get_city_ref(city=city)
         req = self.send(method='getStreet', method_props={"CityRef": city_ref})
@@ -119,36 +131,42 @@ class Address(NovaPoshtaApi):
 
     def get_streets(self, city_ref):
         """
-        Method for parsing all streets data at provided city.
-        API method: `getStreet`.
-        API method properties: `CityRef`.
+        Method for fetching info about streets in desired city.
 
-        Usage:
-            address = Address()
-            address.get_streets(city_ref='0006560c-4079-11de-b509-001d92f78698')
-        :param city_ref (str or unicode):
-            string with ID of the target city
+        :example:
+            ``address = Address()``
+            ``address.get_streets(city_ref='0006560c-4079-11de-b509-001d92f78698')``
+        :param city_ref:
+            ID of the target city
+        :type city_ref:
+            str or unicode
         :return:
-            dict: parsed dictionary with streets data
+            dictionary with info about streets
+        :rtype:
+            dict
         """
         req = self.send(method='getStreet', method_props={"CityRef": city_ref})
         return req
 
     def ex_get_street_by_name(self, city, street):
         """
-        Method for parsing specific street data at provided city.
-        API method: `getStreet`.
-        API method properties: `FindByString`, `CityRef`.
+        Method for fetching info about specific street in desired city.
 
-        :Usage:
-            address = Address()
-            address.ex_get_street_by_name(city=u'Здолбунів', street=u'Незалежності')
-        :param city (str or unicode):
-            string with the name of the target city
-        :param street (str or unicode):
-            string with the name of the target street
+        :example:
+            ``address = Address()``
+            ``print address.ex_get_street_by_name(city=u'Здолбунів', street=u'Незалежності')``
+        :param city:
+            name of the target city
+        :type city:
+            str or unicode
+        :param street:
+            name of the street
+        :type street:
+            str or unicode
         :return:
-            dict: parsed dictionary with street data
+            dictionary with info about street
+        :rtype:
+            dict
         """
         city_ref = self.get_city_ref(city=city)
         req = self.send(method='getStreet', method_props={'CityRef': city_ref, 'FindByString': street})
@@ -156,36 +174,42 @@ class Address(NovaPoshtaApi):
 
     def get_street_by_name(self, city_ref, street):
         """
-        Method for parsing specific street data at provided city.
-        API method: `getStreet`.
-        API method properties: `FindByString`, `CityRef`.
+        Method for fetching info about specific street in desired city.
 
-        :Usage:
-            address = Address()
-            address.get_street_by_name(city_ref='0006560c-4079-11de-b509-001d92f78698', street=u'Незалежності')
-        :param city_ref (str or unicode):
-            string with ID of the target city
-        :param street (str or unicode):
-            string with the name of the target street
+        :example:
+            ``address = Address()``
+            ``address.get_street_by_name(city_ref='0006560c-4079-11de-b509-001d92f78698', street=u'Незалежності')``
+        :param city_ref:
+            ID of the target city
+        :type city_ref:
+            str or unicode
+        :param street:
+            name of the target street
+        :type street:
+            str or unicode
         :return:
-            dict: parsed dictionary with street data
+            dictionary with info about street
+        :rtype:
+            dict
         """
         req = self.send(method='getStreet', method_props={"CityRef": city_ref, "FindByString": street})
         return req
 
     def ex_get_warehouses(self, city):
         """
-        Method for parsing all warehouses data at provided city.
-        API method: `getWarehouses`.
-        API method properties: `CityRef`.
+        Method for fetching info about all warehouses in desired city.
 
-        :Usage:
-            address = Address()
-            address.get_warehouses(city=u'Здолбунів')
-        :param city (str or unicode):
-            string with the name of the target city
+        :example:
+            ``address = Address()``
+            ``address.get_warehouses(city=u'Здолбунів')``
+        :param city:
+            name of the target city
+        :type city:
+            str or unicode
         :return:
-            dict: parsed dictionary with all warehouses data
+            parsed dictionary with all info about warehouses
+        :rtype:
+            dict
         """
         city_ref = self.get_city_ref(city=city)
         req = self.send(method='getWarehouses', method_props={'CityRef': city_ref})
@@ -193,63 +217,106 @@ class Address(NovaPoshtaApi):
 
     def get_warehouses(self, city_ref):
         """
-        Method for parsing all warehouses data at provided city.
-        API method: `getWarehouses`.
-        API method properties: `CityRef`.
+        Method for fetching info about all warehouses in desired city.
 
-        :Usage:
-            address = Address()
-            address.get_warehouses(city='0006560c-4079-11de-b509-001d92f78698')
+        :example:
+            ``address = Address()``
+            ``address.get_warehouses(city='0006560c-4079-11de-b509-001d92f78698')``
         :param city_ref:
-            string with ID of the target city
+            ID of the target city
+        :type city_ref:
+            str or unicode
         :return:
-            dict: parsed dictionary with all warehouses data
+            parsed dictionary with all info about warehouses
+        :rtype:
+            dict
         """
         req = self.send(method='getWarehouses', method_props={"CityRef": city_ref})
         return req
 
     def get_warehouse_types(self):
         """
-        Method for parsing warehouse types data .
-        API method: `getWarehouseTypes`.
+        Method for fetching info about warehouse's types.
 
-        :Usage:
-            address = Address()
-            address.get_warehouse_types()
+        :example:
+            ``address = Address()``
+            ``address.get_warehouse_types()``
         :return:
-            dict: parsed dictionary with warehouse types data
+            parsed dictionary with info about warehouse's types
+        :rtype:
+            dict
         """
         req = self.send(method='getWarehouseTypes')
         return req
 
     def get_areas(self):
         """
-        Method for parsing geographical areas data.
-        API method: `getAreas`.
+        Method for fetching info about areas geographical areas.
 
-        :Usage:
-            address = Address()
-            address.get_areas()
+        :example:
+            ``address = Address()``
+            ``address.get_areas()``
         :return:
-            dict: parsed dictionary with areas data
+            parsed dictionary with info about areas
+        :rtype:
+            dict
         """
         req = self.send(method='getAreas')
         return req
 
 
 class Counterparty(NovaPoshtaApi):
+    """
+    A class representing the Address model of Nova Poshta API.
+    Used for interact with counterpart's info.
+    """
+
     def get_counterparties(self, cp_type='Sender'):
+        """
+        Method for fetching all information about counterparties.
+
+        :example:
+            ``counterparty = Counterparty()``
+            ``counterparty.get_counterparties(cp_type='Recipient')``
+        :param cp_type:
+            type of the counterparty: can be either `Sender` or `Recipient` (`Sender` used as default)
+        :type cp_type:
+            str or unicode
+        :return:
+            dictionary with info about counterparties
+        :rtype:
+            dict
+        """
         req = self.send(method='getCounterparties', method_props={"CounterpartyProperty": cp_type})
         return req
 
     def get_counterparty_by_name(self, name, cp_type='Sender'):
+        """
+        Method for fetching info about counterparty by name.
+
+        :example:
+            ``counterparty = Counterparty()``
+            ``counterparty.get_counterparties(name=u'Талісман', cp_type='Recipient')``
+        :param name:
+            name of the desired counterparty
+        :type name:
+            str or unicode
+        :param cp_type:
+            type of the counterparty: can be either `Sender` or `Recipient` (`Sender` used as default)
+        :type cp_type:
+            str or unicode
+        :return:
+            parsed dictionary with info about counterparty
+        :rtype:
+            dict
+        """
         req = self.send(method='getCounterparties',
                         method_props={"CounterpartyProperty": cp_type, 'FindByString': name})
         return req
 
     def get_counterparty_ref(self, name, cp_type='Sender'):
         props = {'CounterpartyProperty': cp_type, 'FindByString': name}  # separate for a better look :)
-        req = self.send(method='getCounterparties', method_props=props)
+        req = self.send(method='getCounterparties', method_props=props)  # TODO: add ref parsing
         return req
 
     def ex_get_counterparty_by_ERDPOU(self, city, code):
