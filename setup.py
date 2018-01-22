6686ca9331b2682b657e293609b98276c54e462c
sfs-osemesch <osemesch@softserveinc.com>
2018-01-22 17:33:53 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from distutils.core import setup
 setup(
     name='novaposhta-api-client',
     version='0.0.9',
-    packages=['requests',
-              'novaposhta', 'novaposhta.api'],
+    packages=['novaposhta', 'novaposhta.api'],
     url='https://github.com/semolex/novaposhta-api-client',
+    install_requires=[
+        'requests',
+    ],
     license='MIT',
     author='semolex (Oleksii Semeshchuk)',
     author_email='semolex@live.com',
